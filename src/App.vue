acd2537f492443dde4b6aea1c88fa1e45ff30d0d
liuyinjiwen06 <liuyinjiwen@163.com>
2024-11-20 01:52:07 +0800

    pic opti

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,12 @@
     <div class="max-w-7xl mx-auto px-4">
       <div class="flex justify-between h-16">
         <div class="flex items-center space-x-28">
-          <h1 class="text-xl font-bold text-[#2C1810]">{{ $t('header.title') }}</h1>
-          
+        <div class="flex items-center space-x-4">
+          <img src="/yikingfavicon/96_96.png" alt="Yi King Tirage" class="w-9 h-9">
+          <router-link to="/">
+            <h1 class="text-xl font-bold text-[#2C1810]">{{ $t('header.title') }}</h1>
+          </router-link>
+        </div>
           <!-- 导航链接 -->
           <div class="flex items-center space-x-8 ">
             <router-link 
